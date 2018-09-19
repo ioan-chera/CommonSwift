e4f7d0c04b11caad11bc3764a893caa3767a20e2
Ioan Chera <ioan.chera@gmail.com>
2018-09-20 00:55:51 +0300

    Initial Commit

---
+++ CommonSwift/CommonSwift.h
@@ -0,0 +1,19 @@
+//
+//  CommonSwift.h
+//  CommonSwift
+//
+//  Created by ioan on 20.09.2018.
+//  Copyright © 2018 Ioan Chera. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CommonSwift.
+FOUNDATION_EXPORT double CommonSwiftVersionNumber;
+
+//! Project version string for CommonSwift.
+FOUNDATION_EXPORT const unsigned char CommonSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonSwift/PublicHeader.h>
+
+
